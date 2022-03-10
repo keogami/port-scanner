65125998a7b01d9406aeac07b8e50c19ff75e0e3
keogami <41939011+keogami@users.noreply.github.com>
2022-03-10 22:47:46 +0530

    added a seed to the RNG

--- scan.go
+++ scan.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-  "math/rand"
+	"math/rand"
+	"time"
 )
+
+func init() {
+  rand.Seed(time.Now().Unix())
+}
 
 func scan(ip string, port int) bool {
   return rand.Intn(2) == 0
