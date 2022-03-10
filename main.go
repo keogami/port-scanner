9858f2cd6035b12b5f48065590ef5d8ac4402fba
emnaruto07 <shazebvps@gmail.com>
2022-03-10 23:24:28 +0530

    removed comments

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import "fmt"
 
 func main() {
 	// looping all ports
-	// result := 0
 	for i := 1; i <= 66534; i++ {
 		result := scan("127.0.0.1", i)
-		// result = i
-		// result += 1
 		fmt.Printf("Port number %d open? => %t \n", i, result)
 	}
 }
