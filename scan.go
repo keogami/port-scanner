d548737dfd160ad89de315b6463e0a047bdc5a8a
emnaruto07 <shazebvps@gmail.com>
2022-03-10 23:23:39 +0530

    main func completed

--- scan.go
+++ scan.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -12,18 +11,8 @@ func init() {
 
 //function to scan the ip and port
 func scan(ip string, port int) bool {
+	_, _ = ip, port
 	return rand.Intn(2) == 0
 }
 
 //main function
-
-func main() {
-	// looping all ports
-	result := 0
-	for i := 1; i <= 66534; i++ {
-		result = scan("127.0.0.1", i)
-		// result = i
-		result += 1
-		fmt.Println(result)
-	}
-}
