21080959f915bc66f793d1c7a92d3410648b9c31
emnaruto07 <shazebvps@gmail.com>
2022-03-10 23:38:56 +0530

    add slice

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	var open []int
+
 	// looping all ports
 	for i := 1; i <= 66534; i++ {
 		result := scan("127.0.0.1", i)
-		fmt.Printf("Port number %d open? => %t \n", i, result)
+
+		if result != false {
+			openPorts := append(open, i)
+			fmt.Printf("Open ports: %d \n", openPorts)
+			// for j := range openPorts {
+			// 	fmt.Printf()
+			// }
+		}
 	}
+
 }
