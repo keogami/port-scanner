e30519088c058bd4f7d48df153877016dcff6444
emnaruto07 <shazebvps@gmail.com>
2022-03-10 23:44:30 +0530

    code improvement

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ func main() {
 
 	// looping all ports
 	for i := 1; i <= 66534; i++ {
-		result := scan("127.0.0.1", i)
+		IsOpen := scan("127.0.0.1", i)
 
-		if result != false {
-			openPorts := append(open, i)
-			fmt.Printf("Open ports: %d \n", openPorts)
+		if IsOpen {
+			open = append(open, i)
+			fmt.Printf("Open ports: %d \n", open)
 			// for j := range openPorts {
 			// 	fmt.Printf()
 			// }
